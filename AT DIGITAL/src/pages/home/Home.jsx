aed2ea0f4ef1ef9d1dbbd95a805c6913de30594b
kavishka <thushalrasingolla99@gmail.com>
2024-08-10 20:20:29 +0530

    service 2 section for 375px screen size is done

--- AT DIGITAL/src/pages/home/Home.jsx
+++ AT DIGITAL/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import { data } from '../../assets/data'
 import hero from '../../assets/hero.png'
 import Button from '../../components/button/Button'
 import image2 from '../../assets/image2.png'
+import image1 from '../../assets/image1.png'
 function Home() {
   const [showId, setShowId] = useState(null)
   return (
@@ -32,6 +33,22 @@ function Home() {
 
         <Button text='LEARN MORE' height={38} width={150} onClick={()=>{}}/>
         </div>
+
+      </div>
+
+      {/* service section 2 */}
+      <div className='flex flex-col my-[60px] mx-[20px] items-center gap-[20px] md:flex-row md:mx-[40px] lg:mx-[152px] lg:my-[80px] xl:mx-[188px] xl:gap-[108px]'>
+      <div className='w-[275px] h-[276px] lg:w-[346px] lg:h-[346px] xl:w-[414px] xl:h-[414px]'>
+          <img src={image1} alt='service 1' className='w-full h-full object-cover'/>
+
+        </div>
+        <div className='flex flex-col gap-[20px] items-center md:flex-1 md:items-start '>
+          <h1 className='text-[27px] font-normal text-Primary text-center font-poppins leading-[33px]  md:text-start' style={{ letterSpacing: '0.4px' }}>Digital Strategy Consulting</h1>
+          <p className='text-[16px] leading-[19.36px] text-center font-thin text-black md:text-start'>Your digital strategy should complement the overall marketing strategy of the company. In online marketing, each component will never work in isolation and every business needs a different mix. We provide a clear concept and strategic overview to find the most efficient model for your business.</p>
+
+        <Button text='LEARN MORE' height={38} width={150} onClick={()=>{}}/>
+        </div>
+        
 
       </div>
 
